6b0f8a2a41bd25bfbc03b83e05b7931995478734
mtesluk <mtesluk@mtesluk.myguest.virtualbox.org>
2023-07-10 13:34:15 +0200

    Init project

---
+++ src/main/java/pl/socials/SocialsApplication.java
@@ -0,0 +1,12 @@
+package pl.socials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialsApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/socials/SocialsApplicationTests.java
@@ -0,0 +1,11 @@
+package pl.socials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialsApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
